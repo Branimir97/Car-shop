50a87b7f9450eb484a298f9eb8afdf4af5e2faf7
Branimir97 <branimirb51@gmail.com>
2020-10-19 15:46:39 +0200

    Added CRUD for Vehicle class

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -16,9 +16,9 @@ class AuthController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+        if ($this->getUser()) {
+             return $this->redirectToRoute('vehicle_index');
+        }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
--- src/Form/VehicleType.php
+++ src/Form/VehicleType.php
@@ -7,7 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class AddVehicleFormType extends AbstractType
+class VehicleType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
