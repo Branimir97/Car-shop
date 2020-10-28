e1dd3398c40959e41c9614a5abd944fcdf35b503
Branimir97 <branimirb51@gmail.com>
2020-10-28 13:27:30 +0100

    Additional Equipment will be edited if exists for any vehicle

--- src/Controller/AdditionalEquipmentController.php
+++ src/Controller/AdditionalEquipmentController.php
@@ -6,6 +6,7 @@ use App\Entity\AdditionalEquipment;
 use App\Entity\Vehicle;
 use App\Form\AdditionalEquipmentFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,26 +20,45 @@ class AdditionalEquipmentController extends AbstractController
      */
     public function index(Request $request)
     {
-        $additional_equipment = new AdditionalEquipment();
         $vehicle_id = $request->get('id');
         $vehicle = $this->getDoctrine()->getRepository(Vehicle::class)->find($vehicle_id);
         $vehicle_title = $vehicle->getMark().' '.$vehicle->getModel();
-        $form = $this->createForm(AdditionalEquipmentFormType::class, $additional_equipment);
-        $form->handleRequest($request);
 
-        if($this->getUser() && $form->isSubmitted() && $form->isValid())
-        {
-            $additional_equipment->setVehicle($vehicle);
+        $savedEquipment = $this->getDoctrine()->getRepository(AdditionalEquipment::class)->findOneBy(
+            ['vehicle'=>$vehicle]
+        );
+
+        if($savedEquipment) {
+
+            $form = $this->createForm(AdditionalEquipmentFormType::class, $savedEquipment);
+            $form->handleRequest($request);
+
+            if ($this->getUser() && $form->isSubmitted() && $form->isValid()) {
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($savedEquipment);
+                $entityManager->flush();
+
+                $this->addFlash('success', 'Additional equipment successfully edited.');
+            }
+            return $this->render('additional_equipment/index.html.twig', [
+                'form' => $form->createView(),
+                'vehicle_title' => $vehicle_title
+            ]);
+        }
+        $additionalEquipment = new AdditionalEquipment();
+        $formNew = $this->createForm(AdditionalEquipmentFormType::class, $additionalEquipment);
+        $formNew->handleRequest($request);
+
+        if ($this->getUser() && $formNew->isSubmitted() && $formNew->isValid()) {
+            $additionalEquipment->setVehicle($vehicle);
             $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($additional_equipment);
+            $entityManager->persist($additionalEquipment);
             $entityManager->flush();
 
             $this->addFlash('success', 'Additional equipment successfully saved.');
-            return $this->redirectToRoute('vehicle_index');
         }
-
         return $this->render('additional_equipment/index.html.twig', [
-            'form' => $form->createView(),
+            'form' => $formNew->createView(),
             'vehicle_title' => $vehicle_title
         ]);
     }
