7cd4c23b05a74b6e791a5668225dd3e4f5ea5038
Branimir97 <branimirb51@gmail.com>
2020-11-03 10:47:51 +0100

    Added MailController

---
+++ src/Controller/MailerController.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+
+use Symfony\Component\Routing\Annotation\Route;
+class MailerController extends AbstractController
+{
+
+    /**
+     * @Route("email", name="mail_send")
+     * @param MailerInterface $mailer
+     * @throws TransportExceptionInterface
+     */
+
+    public function sendEmail(MailerInterface $mailer)
+    {
+        $email = (new Email())
+            ->from('branimir@gmail.com')
+            ->to('abcd@gmail.com')
+            ->subject('Test')
+            ->text('I sent new email')
+            ->twig
+
+        $mailer->send($email);
+        return $this->redirectToRoute('home');
+    }
+}
